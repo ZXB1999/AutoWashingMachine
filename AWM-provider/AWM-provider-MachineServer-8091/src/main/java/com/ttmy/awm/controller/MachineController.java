b95214b34ca583a48889b9589cf2137ae5fa2ceb
ttmy <1104165130@qq.com>
2021-02-04 00:12:31 +0800

    V0.0.5.alpha 解决了项目无法打包发布的问题

--- AWM-provider/AWM-provider-MachineServer-8091/src/main/java/com/ttmy/awm/controller/MachineController.java
+++ AWM-provider/AWM-provider-MachineServer-8091/src/main/java/com/ttmy/awm/controller/MachineController.java
@@ -5,7 +5,6 @@ import com.ttmy.awm.api.pojo.Washingmachine;
 import com.ttmy.awm.service.MachineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
