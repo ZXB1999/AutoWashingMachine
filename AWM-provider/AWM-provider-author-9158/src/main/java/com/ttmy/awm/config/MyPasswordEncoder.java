ec5dec887cfedf99352ce42340fa388acafd809e
ttmy <1104165130@qq.com>
2021-02-06 13:05:00 +0800

    V0.0.8 实现了基于SpringSecurity的Oauth2.0协议的令牌发放 还未实现令牌验证，决定对现有架构进行改造

---
+++ AWM-provider/AWM-provider-author-9158/src/main/java/com/ttmy/awm/config/MyPasswordEncoder.java
@@ -0,0 +1,16 @@
+package com.ttmy.awm.config;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyPasswordEncoder implements PasswordEncoder{
+
+    public String encode(CharSequence charSequence) {
+        return charSequence.toString();
+    }
+
+    public boolean matches(CharSequence charSequence, String s) {
+        return s.equals(charSequence.toString());
+    }
+}
